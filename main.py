0d96764f3afa63616add7d274ad84dbb50a41e03
DhruvChourasia-IITD <dhruvchourasia.iitd@gmail.com>
2026-06-20 23:37:38 +0530

    Update main.py
    Updated the Code to the one for Weather notification

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+phone_no = os.environ,get("PHONE_NO")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+client = Client(account_sid, auth_token)
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast/"
+parameters = {
+    'appid' : api_key,
+    'lat' : 28.55, #Latitude of IIT-Delhi
+    'lon' : 77.19, #Longitude of IIT-Delhi
+    'cnt' : 4
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+will_rain = False
+data = requests.get(url=OWM_Endpoint, params=parameters)
+data.raise_for_status()
+for block in data.json()['list']:
+    if block['weather'][0]['id'] <700:
+        will_rain = True
+if will_rain:
+    message = client.messages.create(
+        body='Bring an umbrella',
+        from_='+16092860312',
+        to=phone_no
+    )
